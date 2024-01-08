daa277cc3fec4bfb38f4d4464f1a7d836d72c4b3
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2024-01-08 20:16:47 +0700

    add PenyediaViewModel.kt

--- app/src/main/java/com/example/mykmti/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/mykmti/model/PenyediaViewModel.kt
@@ -1,2 +1,36 @@
 package com.example.mykmti.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.mykmti.AplikasiAnggota
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiAnggota().container.repositoryAnggota)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiAnggota().container.repositoryAnggota)
+        }
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiAnggota().container.repositoryAnggota,
+            )
+        }
+        initializer {
+            EditViewModel(
+                createSavedStateHandle(),
+                aplikasiAnggota().container.repositoryAnggota
+            )
+        }
+    }
+}
+
+fun CreationExtras.aplikasiAnggota():AplikasiAnggota =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiAnggota)
