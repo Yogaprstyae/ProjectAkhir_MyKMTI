84f8bc7e91f8872f89a966f1b9f59f06423b28fe
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2024-01-08 18:28:44 +0700

    membuat class HomeViewModel

---
+++ app/src/main/java/com/example/mykmti/model/HomeViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.mykmti.model
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mykmti.data.Anggota
+import com.example.mykmti.repository.RepositoryAnggota
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(private val  repositorySiswa: RepositoryAnggota): ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = repositorySiswa.getAllAnggotaStream()
+        .filterNotNull()
+        .map { HomeUiState(listAnggota = it.toList()) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState()
+        )
+
+    data class HomeUiState(
+        val listAnggota: List<Anggota> = listOf()
+    )
+}
