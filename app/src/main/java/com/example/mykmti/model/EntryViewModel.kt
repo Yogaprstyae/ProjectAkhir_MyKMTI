a08b01622934d28692af3c198fba6329b40c3b71
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2024-01-08 19:28:02 +0700

    Menambah EntryViewModel

---
+++ app/src/main/java/com/example/mykmti/model/EntryViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.mykmti.model
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.mykmti.data.Anggota
+import com.example.mykmti.repository.RepositoryAnggota
+
+class EntryViewModel(private val repositoryAnggota: RepositoryAnggota): ViewModel(){
+    var uiStateAnggota by mutableStateOf(UIStateAnggota())
+        private set
+    private fun validasiInput(uiState: DetailAnggota = uiStateAnggota.detailAnggota): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && divisi.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    fun updateUIState(detailAnggota: DetailAnggota) {
+        uiStateAnggota =
+            UIStateAnggota(detailAnggota = detailAnggota, isEntryValid = validasiInput(detailAnggota))
+    }
+    suspend fun saveAnggota() {
+        if (validasiInput()) {
+            repositoryAnggota.insertAnggota(uiStateAnggota.detailAnggota.toAnggota())
+        }
+    }
+}
+data class UIStateAnggota(
+    val detailAnggota: DetailAnggota = DetailAnggota(),
+    val isEntryValid: Boolean = false
+)
+data class DetailAnggota(
+    val id: Int = 0,
+    val nama: String = "",
+    val divisi: String = "",
+    val telpon: String = "",
+)
+fun DetailAnggota.toAnggota(): Anggota = Anggota(
+    id = id,
+    nama = nama,
+    divisi = divisi,
+    telpon = telpon
+)
+fun Anggota.toUIStateAnggota(isEntryValid: Boolean = false): UIStateAnggota = UIStateAnggota(
+    detailAnggota = this.toDetailAnggota(),
+    isEntryValid = isEntryValid
+)
+fun Anggota.toDetailAnggota(): DetailAnggota = DetailAnggota(
+    id = id,
+    nama = nama,
+    divisi = divisi,
+    telpon = telpon
+)
