040cdb0c695081beaf9baffec127fb1433344d8a
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2024-01-11 00:59:02 +0700

    kasih warna

--- app/src/main/java/com/example/mykmti/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/mykmti/halaman/HalamanEntry.kt
@@ -36,6 +36,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
@@ -144,7 +145,9 @@ fun FormInputAnggota(
             leadingIcon = {
                 Icon(imageVector = Icons.Default.Person, contentDescription = null)
             },
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Yellow),
             enabled = enabled,
             singleLine = true
         )
